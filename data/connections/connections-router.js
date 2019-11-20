963bbbcbaa682d671c2ef39df10750196caafd1e
frescocodes <hueydakotah@gmail.com>
2019-11-19 19:51:19 -0500

    extra endpoints and model fixes

--- data/connections/connections-router.js
+++ data/connections/connections-router.js
@@ -8,16 +8,25 @@ const {
   addConnection
 } = require("./connections-model");
 
-router.get("/", (req, res) => {
+const { authMiddleware } = require("../../auth/authenticate-middleware");
+
+router.get("/", authMiddleware, (req, res) => {
   return getAllConnections()
     .then(connections => res.status(200).json(connections))
     .catch(error => res.status(500).json({ message: "internal server error" }));
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id/trips", authMiddleware, (req, res) => {
   const { id } = req.params;
   return getPassengers(id)
     .then(passengers => res.status(200).json(passengers))
+    .catch(error => res.status(500).json({ error: "internal server error" }));
+});
+
+router.get("/:id", authMiddleware, (req, res) => {
+  const { id } = req.params;
+  return getConnectionById(id)
+    .then(connection => res.status(200).json(connection))
     .catch(error => res.status(500).json({ error: "internal server error" }));
 });
 
